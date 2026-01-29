c267d998cbe09aa1bea3bab8a5828af48d0eb95e
muhammadfadly <muhammadfadly.mfd@gmail.com>
2026-01-29 09:37:29 +0700

    templeting

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('root');
+Route::get('{any}', [HomeController::class, 'index'])->name('index');
+
+//Update User Details
+Route::post('/update-profile/{id}', [HomeController::class, 'updateProfile'])->name('updateProfile');
+Route::post('/update-password/{id}', [HomeController::class, 'updatePassword'])->name('updatePassword');
+
+//Language Translation
+Route::get('index/{locale}', [HomeController::class, 'lang']);
+
